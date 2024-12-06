aeddc3410aaaab31b406bab8bd5ac16161badb36
sangeethakandikonda <136488630+sangeethakandikonda@users.noreply.github.com>
2024-12-06 22:27:26 +0530

    Add files via upload

---
+++ vamsi.js
@@ -0,0 +1,27 @@
+export const Grounds = [{
+    "id": "1",
+    "Game": "Cricket",
+    "image": "/One/Grounds/1.jpg",
+    "price": "1",
+    "description": "To play cricket"
+},
+{
+    "id": "2",
+    "Game": "football",
+    "image": "/One/Grounds/2.jpg",
+    "price": "2500",
+    "description": "To play Football"},
+{
+    "id": "3",
+    "Game": "Cricket",
+    "image": "/One/Grounds/3.jpg",
+    "price": "1500",
+    "description": "To play cricket"},
+{
+    "id": "4",
+    "Game": "Kabaddi",
+    "image": "/One/Grounds/4.jpg",
+    "price": "500",
+    "description": "To play kabaddi"
+},
+]
